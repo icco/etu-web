0053c7ae07841fb08e82381ca1fa74efea09de1a
Nat Welch <nat@natwelch.com>
2026-01-27 02:30:01 +0000

    fix: styling improvement

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  poweredByHeader: false,
+  productionBrowserSourceMaps: true,
+  reactStrictMode: true,
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
