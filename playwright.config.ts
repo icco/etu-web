3821e7c16d6266e9da89483d480666118acf9dbb
Nat Welch <nat@natwelch.com>
2026-02-04 16:58:17 +0000

    typo

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
-    baseURL: "http://localhost:8080",
+    baseURL: "http://localhost:3000",
     trace: "on-first-retry",
   },
   projects: [
@@ -23,7 +23,7 @@ export default defineConfig({
   ],
   webServer: {
     command: "E2E_MOCK=true yarn dev",
-    url: "http://localhost:8080",
+    url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     env: {
       E2E_MOCK: "true",
