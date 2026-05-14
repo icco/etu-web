ca19ff5f5e15059ceec8fe43f064bc7f5dcec567
Nat Welch <nat@natwelch.com>
2026-05-14 01:26:21 +0000

    fix: upgrade eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,27 @@
-
+import eslintJs from "@eslint/js";
+import eslintReact from "@eslint-react/eslint-plugin";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 
-export default [
+export default tseslint.config(
+  { ignores: ["dist/**", "node_modules/**", ".next/**", "src/generated/**", ".github/scripts/**"] },
   {
-    ignores: ["dist/**", "node_modules/**", ".next/**", "src/generated/**", ".github/scripts/**"]
-  },
-  {languageOptions: { globals: globals.browser }},
-  ...tseslint.configs.recommended,
-  {
-    ...pluginReact.configs.flat.recommended,
-    settings: {
-      react: {
-        version: "19"
-      }
-    }
-  },
-  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      eslintJs.configs.recommended,
+      tseslint.configs.recommended,
+      eslintReact.configs["recommended-typescript"],
+    ],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
     rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-      "@typescript-eslint/no-unused-vars": ["error", { 
-        "argsIgnorePattern": "^_",
-        "varsIgnorePattern": "^_"
-      }]
-    }
-  }
-];
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+    },
+  },
+);
